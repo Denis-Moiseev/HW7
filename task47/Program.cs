f8223bd50a3429dcd27bcf29f24431dd2608cd26
Denis-Moiseev <mrtesla666@gmail.com>
2023-07-27 18:11:38 +0800

    finishTask47

---
+++ task47/Program.cs
@@ -0,0 +1,35 @@
+﻿void Print2DArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t", -1);
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] Create2DArray(int rows, int columns, double startValue, double finishValue)
+{
+    double[,] matrix = new double[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(new Random().NextDouble() * (finishValue - startValue) + startValue, 1);
+        }
+    }
+    return matrix;
+}
+
+int GetInput(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int m = GetInput("Введите количество строк массива: ");
+int n = GetInput("Введите количество столбцов массива: ");
+double[,] array = Create2DArray(m, n, -9.9, 9.9);
+Print2DArray(array);
