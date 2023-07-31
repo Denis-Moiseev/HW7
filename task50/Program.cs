c7a4de742c82915c77c0702c978d244c49aa5ee4
Denis-Moiseev <mrtesla666@gmail.com>
2023-08-01 06:07:47 +0800

    finish

--- task50/Program.cs
+++ task50/Program.cs
@@ -21,39 +21,39 @@
 }
 
 void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            for (int j = 0; j < array.GetLength(1); j++)
-            {
-                Console.Write($"{array[i, j]}\t", -1);
-            }
-            Console.WriteLine();
+            Console.Write($"{array[i, j]}\t", -1);
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Create2DArray(int rows, int columns, int startValue, int finishValue)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(startValue, finishValue + 1);
         }
     }
+    return matrix;
+}
 
-    int[,] Create2DArray(int rows, int columns, int startValue, int finishValue)
-    {
-        int[,] matrix = new int[rows, columns];
-        for (int i = 0; i < matrix.GetLength(0); i++)
-        {
-            for (int j = 0; j < matrix.GetLength(1); j++)
-            {
-                matrix[i, j] = new Random().Next(startValue, finishValue + 1);
-            }
-        }
-        return matrix;
-    }
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
 
-    int GetInput(string text)
-    {
-        Console.Write(text);
-        return Convert.ToInt32(Console.ReadLine());
-    }
-
-    int m = GetInput("Введите количество строк массива: ");
-    int n = GetInput("Введите количество столбцов массива: ");
-    int num = GetInput("Введите искомое число: ");
-    int[,] array = Create2DArray(m, n, 1, 9);
-    Print2DArray(array);
-    PrintNumPosition(array, num);
+int m = GetInput("Введите количество строк массива: ");
+int n = GetInput("Введите количество столбцов массива: ");
+int num = GetInput("Введите искомое число: ");
+int[,] array = Create2DArray(m, n, 1, 9);
+Print2DArray(array);
+PrintNumPosition(array, num);
