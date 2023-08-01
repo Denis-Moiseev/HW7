17eecf44f66e99c962bda17737f71611777d8904
Denis-Moiseev <mrtesla666@gmail.com>
2023-08-01 09:00:04 +0800

    finish

---
+++ task52/Program.cs
@@ -0,0 +1,52 @@
+﻿void ArithmeticEachColumn(int[,] array)
+{
+    Console.Write("Среднее арифметическое каждого столбца: ");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        int sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+        }
+        double average = Math.Round((double)sum / array.GetLength(0), 1);
+        if (j < array.GetLength(0)) Console.Write($"{average}; ");
+        else Console.Write($"{average}.");
+    }
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Create2DArray(int rows, int columns, int startValue, int finishValue)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(startValue, finishValue + 1);
+        }
+    }
+    return matrix;
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int m = GetInput("Введите количество строк массива: ");
+int n = GetInput("Введите количество столбцов массива: ");
+int[,] array = Create2DArray(m, n, 1, 9);
+Print2DArray(array);
+ArithmeticEachColumn(array);
